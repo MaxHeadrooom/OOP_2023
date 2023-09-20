54c271514522380edf9fc0c6d97d48828ee5422f
Esmedlyaev Fedor <89976445+MaxHeadrooom@users.noreply.github.com>
2023-09-21 00:10:36 +0200

    gichagacha

--- lab1/func.h
+++ lab1/func.h
@@ -1,7 +1,27 @@
-#pragma once
-
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-bool func(string);
+bool func(string& s)
+{
+    stack<char>st;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            st.push('(');
+        }
+        else
+        {
+            if (st.size() != 0)
+                st.pop();
+            else
+                return false;
+        }
+    }
+    if (st.size() == 0)
+        return true;
+    else
+        return false;
+}
--- lab1/tests.cpp
+++ lab1/tests.cpp
@@ -1,20 +1,52 @@
 #include "gtest/gtest.h"
-#include "raschet.cpp"
 #include "func.h"
 
 using namespace std;
 
-namespace my{
+string s;
 
 TEST(test01, basic_test_set)
 {
-    ASSERT_TRUE(func("()")==true);
+    s = "()";
+    ASSERT_TRUE(func(s)==true);
 }
+
 TEST(test02, basic_test_set)
 {
-    ASSERT_TRUE(func("(")==false);
+    s = "(";
+    ASSERT_TRUE(func(s)==false);
 }
+
+TEST(test03, basic_test_set)
+{
+    s = ")(())";
+    ASSERT_TRUE(func(s)==false);
 }
+
+TEST(test04, basic_test_set)
+{
+    s = "(())((()())())";
+    ASSERT_TRUE(func(s)==true);
+}
+
+TEST(test05, basic_test_set)
+{
+    s = "";
+    ASSERT_TRUE(func(s)==true);
+}
+
+TEST(test06, basic_test_set)
+{
+    s = "(((((((((((((((((((((((((((((((((((((((((((((((((())))))))))))))))))))))))))))))))))))))))))))))))))";//100скобок
+    ASSERT_TRUE(func(s)==true);
+}
+
+TEST(test07, basic_test_set)
+{
+    s = ")((((((((((((((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))))))))))))))))";
+    ASSERT_TRUE(func(s)==false);
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
