57cdd6bcf9db675627cd9f7c9628f5bfb03fd3aa
Esmedlyaev Fedor <89976445+MaxHeadrooom@users.noreply.github.com>
2023-09-21 13:19:31 +0200

    Add files via upload

---
+++ Lab1/tests.cpp
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+#include "func.h"
+
+using namespace std;
+
+string s;
+
+TEST(test01, basic_test_set)
+{
+    s = "()";
+    ASSERT_TRUE(funcii(s)==true);
+}
+
+TEST(test02, basic_test_set)
+{
+    s = "(";
+    ASSERT_TRUE(funcii(s)==false);
+}
+
+TEST(test03, basic_test_set)
+{
+    s = ")(())";
+    ASSERT_TRUE(funcii(s)==false);
+}
+
+TEST(test04, basic_test_set)
+{
+    s = "(())((()())())";
+    ASSERT_TRUE(funcii(s)==true);
+}
+
+TEST(test05, basic_test_set)
+{
+    s = "";
+    ASSERT_TRUE(funcii(s)==true);
+}
+
+TEST(test06, basic_test_set)
+{
+    s = "(((((((((((((((((((((((((((((((((((((((((((((((((())))))))))))))))))))))))))))))))))))))))))))))))))";//100скобок
+    ASSERT_TRUE(funcii(s)==true);
+}
+
+TEST(test07, basic_test_set)
+{    s = ")((((((((((((((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))))))))))))))))";
+    ASSERT_TRUE(funcii(s)==false);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
